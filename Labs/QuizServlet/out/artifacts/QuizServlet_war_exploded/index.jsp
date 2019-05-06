586e71373eda1a2f391483593d7ad6941e890660
babita6 <babita.lamichhane6@gmail.com>
2019-05-06 00:49:03 -0500

    123

---
+++ Labs/QuizServlet/out/artifacts/QuizServlet_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Babita
+  Date: 5/5/2019
+  Time: 11:53 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Servlet Quiz</title>
+  </head>
+  <body>
+  go through http://localhost:8080/QuizServlet/home
+  </body>
+</html>
---
+++ Labs/QuizServlet/src/main/java/QuizServlet.java
@@ -0,0 +1,69 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/home")
+public class QuizServlet extends HttpServlet {
+
+
+        private Quiz quiz = new Quiz();
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            genQuizPage(quiz, resp.getWriter(),"1",false,"");
+        }
+
+        @Override
+        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            String answer =req.getParameter("txtAnswer");
+
+            boolean isCorrect = quiz.isCorrect(answer);
+            boolean error = false;
+            if(isCorrect){
+                quiz.scoreAnswer();
+            }else{
+                error=true;
+            }
+
+            genQuizPage(quiz, resp.getWriter(), quiz.getCurrentQuestion(),error, answer);
+        }
+
+        private void genQuizPage(Quiz sessQuiz, PrintWriter out, String currQuest, boolean error, String answer) {
+
+            out.print("<html>");
+            out.print("<head>");
+            out.print("	<title>NumberQuiz</title>");
+            out.print("</head>");
+            out.print("<body>");
+            out.print("	<form method='post'>");
+            out.print("		<h3>Have fun with NumberQuiz!</h3>");
+            out.print("<p>Your current score is: ");
+            out.print(sessQuiz.getNumberCorrect() + "</br></br>");
+            out.print("<p>Guess the next number in the sequence! ");
+            out.print(currQuest + "</p>");
+
+            out.print("<p>Your answer:<input type='text' name='txtAnswer' value='' /></p> ");
+
+            /* if incorrect, then print out error message */
+            if (error && (answer != null)) {  //REFACTOR?-- assumes answer null only when first open page
+                out.print("<p style='color:red'>Your last answer was not correct! Please try again</p> ");
+            }
+            out.print("<p><input type='submit' name='btnNext' value='Next' /></p> ");
+
+            out.print("</form>");
+            out.print("</body></html>");
+        }
+
+        private void genQuizOverPage(PrintWriter out) {
+            out.print("<html> ");
+            out.print("<head >");
+            out.print("<title>NumberQuiz is over</title> ");
+            out.print("</head> ");
+            out.print("<body> ");
+            out.print("<p style='color:red'>The number quiz is over!</p></body> ");
+            out.print("</html> ");
+        }
+}
