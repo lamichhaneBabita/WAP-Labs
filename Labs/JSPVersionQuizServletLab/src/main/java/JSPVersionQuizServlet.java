2df5bb9351011275771d344135ce0e495a03201f
babita6 <babita.lamichhane6@gmail.com>
2019-05-06 23:49:38 -0500

    123

---
+++ Labs/JSPVersionQuizServletLab/src/main/java/JSPVersionQuizServlet.java
@@ -0,0 +1,76 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import  java.lang.Throwable;
+
+@WebServlet("/jspQuiz")
+
+public class JSPVersionQuizServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        Quiz sessQuiz = (Quiz) request.getSession().getAttribute("quiz");
+        if (sessQuiz == null) {
+            sessQuiz = new Quiz();
+            request.getSession().setAttribute("quiz", sessQuiz);
+        }
+
+        String answer = request.getParameter("txtAnswer");
+
+        if(request.getMethod().equals("POST")) {
+            String isCheckAge = (String)request.getSession().getAttribute("isValidAge");
+
+            if(isCheckAge==null||isCheckAge=="N") {
+                try {
+                    String playerAge = request.getParameter("txtAge");
+                    int age = Integer.parseInt(playerAge);
+                    if (!(age >= 4 && age <= 100))
+                        throw new Exception("Invalid Age to play!");
+                    request.getSession().setAttribute("isValidAge", "Y");
+                } catch (Exception ex) {
+                    request.getSession().setAttribute("isValidAge", "N");
+                }
+            }
+
+        }
+
+
+        RequestDispatcher dispatcher;
+
+
+
+
+
+        boolean error = true;
+        if ((answer != null) && sessQuiz.isCorrect(answer)) {
+            error = false;
+            sessQuiz.scoreAnswer();
+        }
+
+        request.setAttribute("error", error);
+        request.setAttribute("answer", answer);
+
+        if (sessQuiz.getNumberCorrect() < sessQuiz.getNumQuestions())
+            dispatcher = request.getRequestDispatcher("quizpage.jsp");
+        else
+            dispatcher = request.getRequestDispatcher("quizoverpage.jsp");
+
+        dispatcher.forward(request, response);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
+
+
+
---
+++ Labs/JSPVersionQuizServletLab/web/quizoverpage.jsp
@@ -0,0 +1,54 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Babita
+  Date: 5/6/2019
+  Time: 10:51 PM
+  To change this template use File | Settings | File Templates.
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+<head>
+  <title>NumberQuiz</title>
+  <script>
+    function hintClick() {
+      //e.preventDefault();
+      alert(${quiz.getHint(quiz.getCurrentQuestionIndex())});
+
+    }
+  </script>
+</head>
+<body>
+<form method='post' action="jspQuiz">
+
+  <c:if test="${isValidAge=='N' || isValidAge==null}">
+    <input type="text" name="txtAge" value=""/>
+    <c:if test="${isValidAge=='N'}">
+      <label style="color: red">Invalid Age.</label>
+    </c:if>
+    <p><input type='submit' value='Submit Age'/></p>
+  </c:if>
+
+  <c:if test="${isValidAge=='Y'}">
+    <h3>Have fun with NumberQuiz!!</h3>
+    <p>Your current score is:
+        ${quiz.getNumberCorrect()} </p>
+
+    <p>Guess the next number in the sequence! ${quiz.getCurrentQuestion()}</p>
+
+    <input type='button' value='Hint' onclick="hintClick()" style="font-size: xx-small"/>
+
+    <p>Your answer:<input type='text' name='txtAnswer' value=''/></p>
+
+    <c:if test="${error && answer!=null}">
+
+      <p style='color:red'>Your last answer was not correct! Please try again</p>
+
+    </c:if>
+
+    <p><input type='submit' value='Next'/></p>
+  </c:if>
+</form>
+</body>
+</html>
