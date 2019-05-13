14a1f3c964190f5d912569c7090e87b111939a4a
babita6 <babita.lamichhane6@gmail.com>
2019-05-13 16:32:07 -0500

    123

---
+++ JavaScript/AsciiAnimation/ascii.js
@@ -0,0 +1,17 @@
+(function(){
+let btnStart;
+let btnStop;
+let txtShowAnimation;
+
+window.onload = function(){
+    txtShowAnimation = document.getElementById("txtShowAnimation");
+    btnStart = document.getElementById("btnStart");
+    btnStart.onclick = startAnimation;
+
+};
+
+function startAnimation(){
+    
+}
+
+})();
