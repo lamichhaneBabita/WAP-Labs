2fb84d8b11a117fb62059e56baa2dd7f1dce44d6
babita6 <babita.lamichhane6@gmail.com>
2019-05-13 00:51:20 -0500

    123

--- JavaScript/DecorateMyText/decoratemytext_files/decoratemytext.js
+++ JavaScript/DecorateMyText/decoratemytext_files/decoratemytext.js
@@ -5,17 +5,30 @@ let timerId = null;
 window.onload = function(){
     textarea = document.getElementById("text");
     document.getElementById("biggerDecoration").onclick=setBiggerDecoration;
-    document.getElementById("checkBox").onchange = blingText;
+    document.getElementById("checkBox").onchange = changeBling;
 };
+
 function biggerDecoration(){
    let fontsize= window.getComputedStyle(textarea, null).getPropertyValue("font-size");
    textarea.style.fontSize=parseInt(fontsize)+ 2 + "px";
 }
-function blingText(){
-  let blingtext=  document.getElementById("text");
-  blingtext.style.color = "green";
-  blingtext.style.textDecoration = "underline";
 
+function changeBling(){
+
+  let changeText=  document.getElementById("checkBox");
+    if(changeText.checked){
+        textarea.style.fontWeight = "bold";
+        textarea.style.color = "green";
+        textarea.style.textDecoration = "underline";
+        document.body.style.backgroundImage = "url('hundred-dollar-bill.jpg')";
+    }
+    else{
+        textarea.style.fontSize = "normal";
+        textarea.style.color="black";
+        textarea.style.textDecoration = "none";
+        document.body.style.backgroundImage = "none";
+    }
+  
 }
 function setBiggerDecoration(){
     if(timerId){
