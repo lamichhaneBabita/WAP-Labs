42ebc1737b6f19184fd526aa6214fc4b028f89e9
babita6 <babita.lamichhane6@gmail.com>
2019-05-15 14:47:25 -0500

    123

---
+++ JavaScript/ClousersandModulePattern/banking/timer.js
@@ -0,0 +1,11 @@
+
+ 
+
+var f = (function(){
+    return function(){
+        setInterval(() => {
+            console.log("Rudy")
+        }, 1000);
+    };
+})();
+document.getElementById("timerset").onclick = f();
