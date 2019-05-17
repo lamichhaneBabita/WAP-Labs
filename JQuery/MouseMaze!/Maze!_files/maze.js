23056f06c4965c20d9f1a98e4798e0c1b90fdcc0
babita6 <babita.lamichhane6@gmail.com>
2019-05-16 23:26:24 -0500

    123

--- JQuery/MouseMaze!/Maze!_files/maze.js
+++ JQuery/MouseMaze!/Maze!_files/maze.js
@@ -1,14 +1,47 @@
 (function(){
     "use strict";
     $(function(){
+        $("#start").click(reset);
         $(".boundary").mouseover(turnRed);
+        $("#gameArea").mouseleave(cheating);
+        $("#end").mouseover(result);
     });
     let disqualified = false;
     let gameComplete = false;
 
+    function reset(){
+        $(".boundary").removeClass("youLoose");
+        disqualified=false;
+        gameComplete = false;
+        $("#status").html("Click the S to begin.").css("color", "black");
+    }
     function turnRed(){
-        $(".boundary").addClass("You loose");
+        $(".boundary").addClass("YouLoose");
         disqualified =true;
     }
+
+    function result(){
+       if(!disqualified)
+           won();
+          else
+          lost(); 
+       
+    }
+    function won(){
+        $("#status").css("color", "Green").html("Congratulations, You Won!! Click S to play Again.");
+        gameComplete=true;
+    }
+    function lost(){
+        $("#status").html("Sorry, You Loose!! :( please Click S to plain Again." ).css("color", "Red");
+        gameComplete = false;
+
+    }
+    function cheating(){
+       if(!gameComplete){
+           turnRed();
+           lost();
+       }
+    }
+    
 
 });
