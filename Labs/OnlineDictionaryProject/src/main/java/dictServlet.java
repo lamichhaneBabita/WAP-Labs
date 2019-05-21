2f260338c930e2bd4242fe95e48b937d82860cbb
babita6 <babita.lamichhane6@gmail.com>
2019-05-21 00:22:17 -0500

    123

--- Labs/OnlineDictionaryProject/src/main/java/dictServlet.java
+++ Labs/OnlineDictionaryProject/src/main/java/dictServlet.java
@@ -29,7 +29,6 @@ public class dictServlet extends HttpServlet {
 
     @Override
 
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         request.setCharacterEncoding("UTF-8");
